5293697cf389bef8c1c8941a6acba35aab9c3e63
Sudhanshu Gautam <122380056+Sudhanshu-8@users.noreply.github.com>
2025-03-20 01:11:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.use(express.json()); // Ensure JSON parsing
 
 // ✅ MySQL Connection (Using Railway Database)
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  host: yamabiko.proxy.rlwy.net,
+  user: root,
+  password: ddrBcfCtHuESrHlYtUGbDepigbAGrxpu,
+  database: railway,
+  port: 23513
 });
 
 db.connect((err) => {
